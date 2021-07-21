2988b5e8e6bb6d4c99badac20dfe7a65c2b44366
SandorSt <sansteinhardt@gmail.com>
2021-07-21 10:52:10 -0300

    gomix

--- myApp.js
+++ myApp.js
@@ -1,39 +1,17 @@
 var express = require('express');
 var app = express();
 
+console.log('Hello Word')
+
+absolutePath = __dirname + '/views/index.html'
+
+app.get('/', function(req, res){
+    res.sendFile(absolutePath);
+})
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use(express.static(__dirname + '/public'))
 
 
  module.exports = app;
